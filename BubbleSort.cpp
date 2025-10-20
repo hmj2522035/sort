eafcae7984729ab1d983a0d1cf4db1789ffc68f3
DESKTOP-KG3VVFC\student <a>
2025-10-20 11:49:08 +0900

    BubbleSort追加

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,5 +2,13 @@
 
 void BubbleSort::Exec(int* array, int size)
 {
-
+	for (int i = 0; i < size; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (array[i] > array[j]) {
+				int number = array[i];
+				array[i] = array[j];
+				array[j] = number;
+			}
+		}
+	}
 }
